c02ec53d94b310e25c9e68915d0dab01c365cce6
idil-abdi <75079587+idil-abdi@users.noreply.github.com>
2024-10-08 15:12:34 +0000

    changes to my portfolio

--- asset/index.js
+++ asset/index.js
@@ -6,3 +6,21 @@ function hamburgerBar () {
         x.style.display = 'block';
     }
 }
+
+// form validation
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error') 
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if(name.value === '' || name.value == null ) {
+        messages.push('Name is required')
+    }
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+    
+})
