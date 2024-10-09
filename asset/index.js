5dd0b4b6225a2f3c30bb8b6021afabbae992e69c
idil-abdi <75079587+idil-abdi@users.noreply.github.com>
2024-10-09 08:26:51 +0000

    completed contact me section

--- asset/index.js
+++ asset/index.js
@@ -8,19 +8,58 @@ function hamburgerBar () {
 }
 
 // form validation
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const form = document.getElementById('form');
-const errorElement = document.getElementById('error') 
+const nameError = document.getElementById('name-error')
+const emailError = document.getElementById('email-error')
+const messageError = document.getElementById('message-error')
+const submitError = document.getElementById('submit-error')
 
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if(name.value === '' || name.value == null ) {
-        messages.push('Name is required')
+function validateName() {
+  const name = document.getElementById('contact-name').value;
+  if(name.length == 0){
+    nameError.innerHTML = 'Enter full name.';
+    return false;
+  }
+  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    nameError.innerHTML = 'Write full name';
+    return false;
+  }
+  nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateEmail() {
+    const email = document.getElementById('contact-email').value;
+    if(email.length == 0){
+        emailError.innerHTML = 'Enter email address.';
+        return false;
     }
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email Invalid';
+        return false;
     }
-    
-})
+      emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+      return true;
+}
+
+function validateMessage() {
+    const message = document.getElementById('contact-message').value;
+    const required = 30;
+    const left = required - message.length;
+
+    if (left > 0) {
+        messageError.innerHTML = left + ' more characters required';
+    return false;        
+    }
+
+    messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateForm() {
+    if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage() ) {
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fix error to submit';
+        setTimeout(function(){submitError.style.display = 'none';}, 3000)
+        return false;
+    }
+}
