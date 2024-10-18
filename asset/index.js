deae3b88e25950df7b9ecb3acaddd6687055f622
idil-abdi <75079587+idil-abdi@users.noreply.github.com>
2024-10-18 08:18:09 +0000

    update

--- asset/index.js
+++ asset/index.js
@@ -140,3 +140,17 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const hiddenElement = document.querySelectorAll('.hidden')
+hiddenElement.forEach((el) => observer.observe(el))
